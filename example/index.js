d4d3fd1556ad12212093134867b96c622b6da127
Ionică Bizău <bizauionica@gmail.com>
2015-10-04 10:35:59 +0300

    Added example

---
+++ example/index.js
@@ -0,0 +1,37 @@
+// Dependencies
+var NameIt = require("../lib");
+
+// Get a name for "name"
+console.log(NameIt("name"));
+// =>
+// { _:
+//    [ 'namy'
+//    , 'namer'
+//    , 'name2'
+//    , 'namify'
+//    , 'name-it'
+//    , 'name.js'
+//    , 'namous'
+//    , 'namly'
+//    , 'namance'
+//    , 'namoid'
+//    , 'namient'
+//    , 'nament'
+//    , 'namonin'
+//    , 'namoxin'
+//    , 'namo ]
+// , y: 'namy'
+// , er: 'namer'
+// , two: 'name2'
+// , ify: 'namify'
+// , it: 'name-it'
+// , js: 'name.js'
+// , ous: 'namous'
+// , ly: 'namly'
+// , ance: 'namance'
+// , oid: 'namoid'
+// , ient: 'namient'
+// , ent: 'nament'
+// , onin: 'namonin'
+// , oxin: 'namoxin'
+// , o: 'namo' }
